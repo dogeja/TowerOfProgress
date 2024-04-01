7d1757ba22e8815033feb018301dad6c3c506467
dogeja <disk6473@gmail.com>
2024-04-01 21:29:07 +0900

    0401 add: firebase&.env

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const KEY = process.env.API_KEY;
+
+const firebaseConfig = {
+  apiKey: `${KEY}`,
+  authDomain: "towerofprogress-da984.firebaseapp.com",
+  projectId: "towerofprogress-da984",
+  storageBucket: "towerofprogress-da984.appspot.com",
+  messagingSenderId: "185301771931",
+  appId: "1:185301771931:web:d2cdb35e91b8c53260083e",
+  measurementId: "G-42ST63CNW2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
